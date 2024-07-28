f533aca4cf32fa93c46761b6b704fde48ae66872
junaidnjc2 <junaidnjc2@gmail.com>
2024-07-28 18:29:41 +0500

    initial commit

---
+++ main.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "vpc-cidr-block" {}
+variable "subnet-cidr-block" {}
+variable "cidr-blocks" {
+  type = list(string)
+}
+variable "cidr-block-object" {
+  type = list(object({
+    cidr-block = string
+    name = string
+  }))
+  
+}
+  
+resource "aws_vpc" "development-vpc" {
+  cidr_block = "50.0.0.0/16"
+
+  tags = {
+    Name: "musa-vpc"
+  }
+}
+
+resource "aws_subnet" "dev-subnet-1" {
+    vpc_id = aws_vpc.development-vpc.id
+    cidr_block = "50.0.10.0/24"
+    availability_zone = "us-east-1a" 
+
+    tags = {
+      Name: "musa-subnet-1"
+    }
+}
+
+output "dev-vpc" {
+  value = aws_vpc.development-vpc.id
+   
+}
+
+output "dev-subnet" {
+  value = aws_subnet.dev-subnet-1.id
+}
